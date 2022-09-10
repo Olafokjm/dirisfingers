23acc88b65e35379e3b24793d910572bb6847488
Kevin Julio M <97838003+Olafokjm@users.noreply.github.com>
2022-09-10 18:05:51 -0500

    Add files via upload

---
+++ js/alert.js
@@ -0,0 +1,3 @@
+window.addEventListener("load", function() {
+    alert('SECURITY NOTICE: We take the security of our platform and our users very seriously, which is why we comply with the highest security standards in the industry. We never share your data with anyone, we only use it for the purpose of sending orders and communication belonging to them, Fingers DIris Never! will contact you for other purposes.');
+  });
